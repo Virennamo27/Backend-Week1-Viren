7904c2378a78ea4e647800b7665cd87038161f2a
Viren Namo <virennamo27@gmail.com>
2025-09-19 23:12:57 +0530

    Updated <filename> with new changes

--- Flask-App/flask_app.py
+++ Flask-App/flask_app.py
@@ -1,13 +1,42 @@
 from flask import Flask, jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 @app.route("/hello")
 def hello():
+    """
+    A simple hello endpoint.
+    ---
+    responses:
+      200:
+        description: Returns a greeting
+        examples:
+          string: "Hello, Interns!"
+    """
     return "Hello, Interns!"
 
 @app.route("/students")
 def students():
+    """
+    Get list of students
+    ---
+    responses:
+      200:
+        description: A list of students
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              id:
+                type: integer
+              name:
+                type: string
+              age:
+                type: integer
+    """
     return jsonify([
         {"id": 1, "name": "Alice", "age": 22},
         {"id": 2, "name": "Bob", "age": 20},
